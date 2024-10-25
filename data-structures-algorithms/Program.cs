5bea6c3a35df6fabe9209accd48e57fbe12590ad
theonlywisdom <mailbaah@gmail.com>
2024-10-25 10:38:12 +0100

    implement linear search in an ordered array

--- data-structures-algorithms/Program.cs
+++ data-structures-algorithms/Program.cs
@@ -1,8 +1,30 @@
-﻿internal partial class Program
+﻿
+
+internal partial class Program
 {
     private static void Main(string[] args)
     {
-        PrintEvenNumbers(100);
+        int[] intArr = new int[] { 17, 3, 75, 202, 80 };
+        int searchVal = 75;
+        int valIndex = LinearSearch(intArr, searchVal);
+        Console.WriteLine($"{searchVal} is {(valIndex == -1 ? "not found" : $"at index {valIndex}")}");
+    }
+
+    private static int LinearSearch(int[] intArr, int searchVal)
+    {
+        for (int index = 0; index < intArr.Length; index++)
+        {
+            if (intArr[index] == searchVal)
+            {
+                return index;
+            }
+            else if (intArr[index] > searchVal)
+            {
+                break;
+            }
+        }
+
+        return -1;
     }
 
     public static void PrintEvenNumbers(int max)
--- data-structures-algorithms/obj/Debug/net8.0/data-structures-algorithms.AssemblyInfo.cs
+++ data-structures-algorithms/obj/Debug/net8.0/data-structures-algorithms.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("data-structures-algorithms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eee4a68d44fc72725c0b90c24c342e45c43af3aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ca33d881bf36abdef011ccef2eaa225de92d7ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("data-structures-algorithms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("data-structures-algorithms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
